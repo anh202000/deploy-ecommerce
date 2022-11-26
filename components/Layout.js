909644e0985b955cc3a46b8c431d6875146c09f8
Le Phan tuan Anh <anhroyal110@gmail.com>
2022-11-27 00:37:16 +0700

    fix

--- components/Layout.js
+++ components/Layout.js
@@ -6,10 +6,6 @@ import Mess from "../public/mess.js"
 
 function Layout({ children }) {
 
-    const newPopup = (url) => {
-        window.open(
-            url, 'popUpWindow', 'height=600,width=400,right=10,top=10,resizable=yes,scrollbars=yes,toolbar=yes,menubar=no,location=no,directories=no,status=yes')
-    }
     return (
         <div>
             <div className="container">
@@ -21,9 +17,6 @@ function Layout({ children }) {
             <div className="container">
                 <Notify />
                 <Modal />
-                <div onClick={() => newPopup('https://nextjsblogs.herokuapp.com/')} style={{ position: 'fixed', bottom: '4%', right: '2%', zIndex: 999 }}>
-                    <Mess />
-                </div>
                 <div class="online-indicator" style={{ position: 'fixed', bottom: '4%', right: '2%' }}>
                     <span class="blink"></span>
                 </div>
