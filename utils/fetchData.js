d55dec83dacd83569faa2fe2bad71bdbc128f181
Le Phan tuan Anh <anhroyal110@gmail.com>
2022-11-27 00:28:42 +0700

    fix

--- utils/fetchData.js
+++ utils/fetchData.js
@@ -1,7 +1,8 @@
-const baseUrl = process.env.BASE_URL
+const baseUrl = 'https://deploy-ecommerce-a4ffc9whq-anh202000.vercel.app/'
 
 export const getData = async (url, token) => {
-    const res = await fetch(`api/${url}`, {
+    console.log(process.env, 'process.env')
+    const res = await fetch(`${baseUrl}/api/${url}`, {
         method: 'GET',
         headers: {
             'Authorization': token
@@ -13,7 +14,7 @@ export const getData = async (url, token) => {
 }
 
 export const postData = async (url, post, token) => {
-    const res = await fetch(`api/${url}`, {
+    const res = await fetch(`${baseUrl}/api/${url}`, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
@@ -29,7 +30,7 @@ export const postData = async (url, post, token) => {
 
 
 export const putData = async (url, post, token) => {
-    const res = await fetch(`api/${url}`, {
+    const res = await fetch(`${baseUrl}/api/${url}`, {
         method: 'PUT',
         headers: {
             'Content-Type': 'application/json',
@@ -43,7 +44,7 @@ export const putData = async (url, post, token) => {
 }
 
 export const patchData = async (url, post, token) => {
-    const res = await fetch(`api/${url}`, {
+    const res = await fetch(`${baseUrl}/api/${url}`, {
         method: 'PATCH',
         headers: {
             'Content-Type': 'application/json',
@@ -58,7 +59,7 @@ export const patchData = async (url, post, token) => {
 
 
 export const deleteData = async (url, token) => {
-    const res = await fetch(`api/${url}`, {
+    const res = await fetch(`${baseUrl}/api/${url}`, {
         method: 'DELETE',
         headers: {
             'Content-Type': 'application/json',
